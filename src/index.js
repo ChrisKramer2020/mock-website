bcea066389ff03e369bd1e6fd774b8d4bc5163b6
Christopher Kramer <Hendrix1957@gmail.com>
2017-08-16 20:10:46 -0400

    mock-website

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import './styles/screen.scss'
 
 const main = () => {
-  document.querySelector('h1').textContent += '?'
+  document.querySelector('h1').textContent += ''
+  $(function() {
+  		var selectedClass = "";
+  		$(".fil-cat").click(function(){
+  		selectedClass = $(this).attr("data-rel");
+       $("#portfolio").fadeTo(100, 0.1);
+  		$("#portfolio div").not("."+selectedClass).fadeOut().removeClass('scale-anm');
+      setTimeout(function() {
+        $("."+selectedClass).fadeIn().addClass('scale-anm');
+        $("#portfolio").fadeTo(300, 1);
+      }, 300);
+
+  	});
+  });
+
 }
 
 document.addEventListener('DOMContentLoaded', main)
